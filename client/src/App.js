dcf61de54aaa670f64b819f827e2d8b06c797ff0
Darryl <darrylmangibin@gmail.com>
2019-03-14 23:35:30 +0800

    Add Experience Form interface

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import CreateProfile from './components/create-profile/CreateProfile';
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 if(localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -51,6 +52,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-experience" component={AddExperience} />
               </Switch>
             </div>
             <Footer />
