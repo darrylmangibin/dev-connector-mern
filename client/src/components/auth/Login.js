0865539872fcff5f77681c4fe3e097ccf5798732
Darryl <darrylmangibin@gmail.com>
2019-03-20 13:08:19 +0800

    Vivixx dev

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -54,7 +54,7 @@ class Login extends React.Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <p className="lead text-center">Sign in to your Vivixx-Devs account</p>
               <form onSubmit={this.onSubmit}>
 
                 <TextFieldGroup 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -58,7 +58,7 @@ class Register extends React.Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
+              <p className="lead text-center">Create your Vivixx-Devs account</p>
               <form onSubmit={this.onSubmit}>
 
                 <TextFieldGroup 
